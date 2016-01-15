1fa78837187b87ce3cc83dbf7793842463e95b7a
Shivam jindal <jindalshivam65@gmail.com>
2016-01-15 20:44:35 +0530

    some models created

--- registration/register/models.py
+++ registration/register/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+class Student(models.Model):
+	name = models.CharField()
+	contact = models.CharField()
+	email = models.CharField()
+	college = models.CharField()
+	
